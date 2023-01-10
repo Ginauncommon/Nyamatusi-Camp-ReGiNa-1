f48f57b813c5512b8759e6c1631254eed4a188cb
Gina <117254614+Ginauncommon@users.noreply.github.com>
2023-01-10 12:48:29 +0200

    Add files via upload

---
+++ Nyamatusi Camp ReGiNa/js/index.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navabarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click',() => {
+    navabarLinks.classList.toggle('active')
+})
